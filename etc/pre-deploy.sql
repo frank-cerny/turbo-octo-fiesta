6d2de471e96289fbf3b3ccc721d38456ae9271f8
Frank Cerny <fcerny@localhost.localdomain>
2022-03-21 19:37:09 -0400

    Starting work on update-app.sh script (mhm)

--- etc/pre-deploy.sql
+++ etc/pre-deploy.sql
@@ -1,7 +1,7 @@
 declare
 l_workspace_id number;
 begin
-l_workspace_id := apex_util.find_security_group_id (p_workspace => '<strong>temp</strong>');
+l_workspace_id := apex_util.find_security_group_id (p_workspace => '<strong>DEV_WS</strong>');
 apex_util.set_security_group_id (p_security_group_id => l_workspace_id);
 APEX_UTIL.PAUSE(2);
 end;
--- update-app.sh
+++ update-app.sh
@@ -2,13 +2,29 @@
 # update-app.sh
 # Based on the directroy structure of the application, deploy all changes by using the liquibase change logs
 
-SECURITY_ID_SCRIPT_DIRECTORY=""
+ROOT_DIR="$PWD"
+SECURITY_ID_SCRIPT_DIRECTORY="$ROOT_DIR/etc/"
 LOGIC_CHANGELOG_DIRECTORY=""
 SCHEMA_CHANGELOG_DIRECTORY=""
 TEST_CHANGELOG_DIRECTORY=""
 APP_INSTALL_CHANGELOG_DIRECTORY=""
+DB_IP="192.168.0.81"
+DB_PORT="45001"
+DB_PASSWORD="Passw0rd!"
+
+printf "/etc Path: $SECURITY_ID_SCRIPT_DIRECTORY\n"
+printf "Logic Changelog Dir: $LOGIC_CHANGELOG_DIRECTORY\n"
+printf "Schema Changelog Dir: $SCHEMA_CHANGELOG_DIRECTORY\n"
+printf "Test Changelog Dir: $TEST_CHANGELOG_DIRECTORY\n"
+printf "App Install Dir: $APP_INSTALL_CHANGELOG_DIRECTORY\n"
 
 # Run pre-step which sets the security id of the instance based on the workspace id (not sure why this is needed)
+cd $SECURITY_ID_SCRIPT_DIRECTORY
+sql -s /nolog <<EOF
+CONNECT sys@$DB_IP:$DB_PORT/XEPDB1 as sysdba;
+$DB_PASSWORD
+@pre-deploy.sql
+EOF
 
 # Run the logic changelog
 
