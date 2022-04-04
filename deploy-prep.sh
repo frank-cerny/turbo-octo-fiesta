4f3668e014ab411d5f8af67fab7c5581ebe0292b
Frank Cerny <fcerny@Franks-Mac-mini.local>
2022-04-04 07:36:40 -0400

    Accidently re-exporting the entire app (WIP)

--- deploy-prep.sh
+++ deploy-prep.sh
@@ -3,7 +3,7 @@
 # Performs commands related to exporting the current application state in preparation for build/deploy steps
 
 ROOT_DIR="$PWD"
-APP_EXPORT_DIR="$ROOT_DIR/app/deploy"
+APP_EXPORT_DIR="$ROOT_DIR/app"
 SCHEMA_EXPORT_DIR="$ROOT_DIR/database/current_schema/"
 DB_IP="192.168.0.81"
 DB_PORT="45001"
@@ -20,9 +20,18 @@ echo "Cleaning Current Schema (Remove *.xml files)"
 rm -rf "$SCHEMA_EXPORT_DIR"/**/*.xml
 
 # We use the single file export because I have yet to get the split version of the export to properly import into a new workspace
-echo "Exporting Application (Single File) to f100.xml"
-cd "$APP_EXPORT_DIR"
+echo "Exporting Application (Single File) to f100.xml (Cleaning first)"
+cd "$APP_EXPORT_DIR/deploy"
 sql -s /nolog <<EOF
 CONNECT dev_ws/$USER_DB_PASSWORD@$DB_IP:$DB_PORT/XEPDB1
-lb genobject -type apex -applicationid 100 -skipExportDate -expOriginalIds -expACLAssignments -sql -dir .
+lb genobject -type apex -applicationid 100 -skipExportDate -expOriginalIds -expACLAssignments -dir .
 EOF
+
+# We use the single file export because I have yet to get the split version of the export to properly import into a new workspace
+echo "Exporting Application Source to Diffable Directory (Cleaning first)"
+cd "$APP_EXPORT_DIR/src"
+sql -s /nolog <<EOF
+CONNECT dev_ws/$USER_DB_PASSWORD@$DB_IP:$DB_PORT/XEPDB1
+lb genobject -type apex -applicationid 100 -skipExportDate -expOriginalIds -expACLAssignments -split -dir .
+EOF
+echo "Removing controller.xml"
