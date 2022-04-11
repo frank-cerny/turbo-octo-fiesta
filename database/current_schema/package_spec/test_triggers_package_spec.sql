c0e307c2559fe3c79bf3b1794f44d5fca36ba96d
Frank Cerny <fcerny@Franks-Mac-mini.local>
2022-04-11 07:32:50 -0400

    Adding rest of tool trigger tests

--- database/current_schema/package_spec/test_triggers_package_spec.sql
+++ database/current_schema/package_spec/test_triggers_package_spec.sql
@@ -4,7 +4,14 @@ as
     -- %suite(Test Triggers)
 
     --%test(Test Trigger Insert Project Tool With insert)
-    procedure test_trigger_insert_project_tool_insert;
+    procedure test_trigger_insert_project_tool_insert_single_project;
+    --%test(Test Trigger Insert Project Tool With insert on multiple projects)
+    procedure test_trigger_insert_project_tool_insert_multi_project;
     --%test(Test Trigger Insert Project Tool with upsert)
-    -- procedure test_trigger_insert_project_tool_upsert;
+    procedure test_trigger_insert_project_tool_upsert;
+    --%test(Test Trigger Delete Project Tool)
+    --%throws(-01403)
+    procedure test_trigger_delete_project_tool;
+    --%test(Test Trigger Update Project Tool)
+    procedure test_trigger_update_project_tool;
 end test_triggers;
