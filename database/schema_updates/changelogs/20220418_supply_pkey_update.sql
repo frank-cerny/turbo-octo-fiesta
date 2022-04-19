0f0ec7c3745af8a12cd57e7b50ea248a1635ab9f
Frank Cerny <fcerny@Franks-Mac-mini.local>
2022-04-18 20:41:55 -0400

    Updating schema change to drop if exists

--- database/schema_updates/changelogs/20220418_supply_pkey_update.sql
+++ database/schema_updates/changelogs/20220418_supply_pkey_update.sql
@@ -1,8 +1,17 @@
 --liquibase formatted sql
 
 --changeset fcerny:1
-alter table bsa_single_use_supply
-DROP constraint bsa_single_use_supply_uk;
+declare 
+  l_count integer;
+begin
+    SELECT count(*) INTO l_count FROM USER_CONSTRAINTS 
+    WHERE CONSTRAINT_NAME='BSA_REVENUE_ITEM_UK';
+
+    if l_count > 0 then 
+        execute immediate 'alter table bsa_single_use_supply drop constraint bsa_single_use_supply_uk';
+    end if;
+end;
+/
 
 --changest fcerny:2
 -- Add a new constraint back
@@ -11,8 +20,18 @@ ADD constraint bsa_single_use_supply_uk unique (project_id, name);
 
 --changeset fcerny:3
 -- Do the same for revenue items, dropping first to aid in testing
-alter table bsa_revenue_item
-DROP constraint bsa_revenue_item_uk;
+-- Reference: https://docs.oracle.com/cd/B19306_01/server.102/b14237/statviews_1037.htm#i1576022
+declare 
+  l_count integer;
+begin
+    SELECT count(*) INTO l_count FROM USER_CONSTRAINTS 
+    WHERE CONSTRAINT_NAME='BSA_REVENUE_ITEM_UK';
+
+    if l_count > 0 then 
+        execute immediate 'alter table bsa_revenue_item drop constraint bsa_revenue_item_uk';
+    end if;
+end;
+/
 
 --changeset fcerny:4
 alter table bsa_revenue_item
