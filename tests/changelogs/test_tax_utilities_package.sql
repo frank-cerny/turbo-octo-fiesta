2dbf2f0351edd9fcc77db2b7c34a4c173b58374b
Frank Cerny <fcerny@Franks-Mac-mini.local>
2022-05-29 17:45:24 -0400

    Adding 2021 calculation test

--- tests/changelogs/test_tax_utilities_package.sql
+++ tests/changelogs/test_tax_utilities_package.sql
@@ -4,14 +4,43 @@
 create or replace package test_tax_utilities_package
 as
     -- %suite(Test Tax Utility Package)
+    
+    -- %test(Test 2021 Tax Calculations)
+    procedure test_tax_calculation_2021;
+    -- %test(Test 2022 Tax Calculations)
+    procedure test_tax_calculation_2022;
 end test_tax_utilities_package;
 /
 
 --changeset fcerny:2 runOnChange:true endDelimiter:"/" stripComments:false
 create or replace package body test_tax_utilities_package
 as
-    procedure test_project_id_string_single_project is
+    procedure test_tax_calculation_2021 is
+        taxDue number(10, 2);
     begin
+        -- Test each part of the tax bracket to ensure math holds, computations are straight forward inside the function
+        -- hence we can use a single test instead of breaking it out
+        -- 10% bracket
+        taxDue := taxu.bsa_func_calculate_federal_income_tax_2021(500);
+        ut.expect(taxDue).to_( equal(50) );
+        -- 12% bracket
+        taxDue := taxu.bsa_func_calculate_federal_income_tax_2021(32405);
+        ut.expect(taxDue).to_( equal(3689.60) );
+        -- 22% bracket
+        taxDue := taxu.bsa_func_calculate_federal_income_tax_2021(67800);
+        ut.expect(taxDue).to_( equal(10664.50) );
+        -- 24% bracket 
+        taxDue := taxu.bsa_func_calculate_federal_income_tax_2021(90500);
+        ut.expect(taxDue).to_( equal(15741) );
+        -- 32% bracket (WRONG)
+        taxDue := taxu.bsa_func_calculate_federal_income_tax_2021(175900);
+        ut.expect(taxDue).to_( equal(37115) );
+        -- 35% bracket
+        taxDue := taxu.bsa_func_calculate_federal_income_tax_2021(350600);
+        ut.expect(taxDue).to_( equal(97254.25) );
+        -- 37% bracket
+        taxDue := taxu.bsa_func_calculate_federal_income_tax_2021(750000);
+        ut.expect(taxDue).to_( equal(241572.25) );
     end;
 end test_tax_utilities_package;
 /
