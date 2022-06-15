08558dbfdb3bebeee2f4c69c66416dc9c82f5209
Frank Cerny <fcerny@Franks-Mac-mini.local>
2022-06-14 21:05:53 -0400

    Updating schema from app

--- database/current_schema/package_body/project_utilities_package_body.sql
+++ database/current_schema/package_body/project_utilities_package_body.sql
@@ -12,14 +12,14 @@ as
         revenue number;
         taxDue number;
         BEGIN
-            -- APEX_DEBUG.ENABLE(apex_debug.c_log_level_engine_trace);
+            
             SELECT sum(purchaseprice) into bikeCost from bsa_bike where project_id = projectId;
             SELECT sum(costbasis) into toolCost FROM bsa_view_project_tool WHERE project_id = projectId;
             SELECT sum(unitcost*unitspurchased) into singleUseSupplyCost from bsa_single_use_supply where project_id = projectId;
             SELECT sum(costbasis) into fixedSupplyCost FROM bsa_view_project_fixed_supply WHERE project_id = projectId;
             SELECT sum(costbasis) into nonFixedSupplyCost FROM bsa_view_project_non_fixed_supply WHERE project_id = projectId;
             SELECT sum(saleprice) into revenue FROM bsa_revenue_item WHERE project_id = projectId;
-            -- Check for any null values
+            
             if bikeCost is NULL then
                 bikeCost := 0;
             end if;
@@ -39,17 +39,17 @@ as
                 revenue := 0;
             end if;
             taxDue := taxu.bsa_func_calculate_total_federal_tax(projectId);
-            -- apex_debug.info('Bike Purchase Price: ' || bikeCost);
-            -- apex_debug.info('Tool Cost: ' || toolCost);
-            -- apex_debug.info('Single Use Supply Cost: ' || singleUseSupplyCost);
-            -- apex_debug.info('Fixed Supply Cost: ' || fixedSupplyCost);
-            -- apex_debug.info('Non Fixed Supply Cost: ' || nonFixedSupplyCost);
-            -- apex_debug.info('Revenue: ' || revenue);
-            -- Setting the out value is the same as "returning a value"
+            
+            
+            
+            
+            
+            
+            
             return  (revenue - bikeCost - toolCost - singleUseSupplyCost - fixedSupplyCost - nonFixedSupplyCost - taxDue);
         END;
 
-    -- projectIdString is a colon separated project id string
+    
     function bsa_func_return_project_name_string_from_ids (projectIdString IN varchar2)
     RETURN varchar2
     AS
@@ -59,17 +59,17 @@ as
         projectTitle varchar2(200);
         BEGIN
             projectIdList := apex_string.split(projectIdString, ':');
-            -- Count the number of non-null items in the 1xN table 
+            
             if projectIdList.count = 0 then
                 return 'No Projects Selected';
             end if;
-            -- Iterate through the list of ids to get the name of the project (works on 1 - N ids)
+            
             projectNameString := '';
             for i in 1 .. projectIdList.count loop
                 projectId := TO_NUMBER(projectidList(i));
                 SELECT p.title into projectTitle from bsa_project p where id = projectId;
                 projectNameString := (projectNameString || projectTitle);
-                -- If there are more items in the list, append a comma (when i = count, we have found the last item)
+                
                 if i < projectIdList.count then
                     projectNameString := (projectNameString || ', ');
                 end if;
--- database/current_schema/package_body/tax_utilities_package_body.sql
+++ database/current_schema/package_body/tax_utilities_package_body.sql
@@ -1,10 +1,10 @@
 
   CREATE OR REPLACE EDITIONABLE PACKAGE BODY "DEV_WS"."TAX_UTILITIES" 
 as
-    -- Validates that the proper prerequsitite data exists in the database to perform the calculation
-    -- Returns:
-    --  0 = false
-    --  1 = true
+    
+    
+    
+    
     function bsa_func_validate_income_params (yearToCheck IN varchar2)
     RETURN number
     AS
@@ -20,7 +20,7 @@ as
             return 0;
     END;
 
-    -- See below, except we should return a string for each year that can be added to a project page
+    
     function bsa_func_calculate_yearly_federal_tax_string (projectId IN int)
     RETURN varchar2
     AS
@@ -30,8 +30,8 @@ as
     BEGIN
         numberOfRevenueItems := 0;
         taxDueOnProject := 0;
-        -- For each year, call a helper function to make updating this easier in the future :)
-        -- 2021
+        
+        
         SELECT count(id) INTO numberOfRevenueItems from bsa_revenue_item WHERE project_id = projectId and EXTRACT(YEAR FROM dateSold) = '2021' and ispending = 'N' and platformsoldon = 'Ebay';
         if numberOfRevenueItems > 0 THEN
             if (bsa_func_validate_income_params('2021') = 1) THEN
@@ -41,7 +41,7 @@ as
                 taxString := '2021 Tax = N/A';
             end if;
         end if;
-        -- 2022
+        
         SELECT count(id) INTO numberOfRevenueItems from bsa_revenue_item WHERE project_id = projectId and EXTRACT(YEAR FROM dateSold) = '2022' and ispending = 'N' and platformsoldon = 'Ebay';
         if numberOfRevenueItems > 0 THEN
             if (bsa_func_validate_income_params('2022') = 1) THEN
@@ -54,7 +54,7 @@ as
         return taxString;
     END;
 
-    -- If isEstimated = 1, then use estimated income value from db, instead of actual
+    
     function bsa_func_calculate_total_federal_tax (projectId IN int)
     RETURN number
     AS
@@ -63,16 +63,16 @@ as
     BEGIN
         numberOfRevenueItems := 0;
         taxDueOnProject := 0;
-        -- For each year, call a helper function to make updating this easier in the future :)
-        -- 2021
+        
+        
         SELECT count(id) INTO numberOfRevenueItems from bsa_revenue_item WHERE project_id = projectId and EXTRACT(YEAR FROM dateSold) = '2021' and ispending = 'N' and platformsoldon = 'Ebay';
         if numberOfRevenueItems > 0 THEN
-            -- Ensure the income table has the correct values for the computation
+            
             if (bsa_func_validate_income_params('2021') = 1) THEN
                 taxDueOnProject := taxDueOnProject + bsa_func_calculate_federal_tax_helper_2021(projectId);
             end if;
         end if;
-        -- 2022
+        
         SELECT count(id) INTO numberOfRevenueItems from bsa_revenue_item WHERE project_id = projectId and EXTRACT(YEAR FROM dateSold) = '2022' and ispending = 'N' and platformsoldon = 'Ebay';
         if numberOfRevenueItems > 0 THEN
             if (bsa_func_validate_income_params('2022') = 1) THEN
@@ -82,7 +82,7 @@ as
         return taxDueOnProject;
     END;
 
-    -- Number of revenue items for 2021 must be greater than 0, otherwise the behavior of this function is undefined
+    
     function bsa_func_calculate_federal_tax_helper_2021 (projectId IN int)
     RETURN number
     AS
@@ -97,25 +97,25 @@ as
     BEGIN
         yearToCompute := '2021';
         taxDueOnProject := 0;
-        -- For each year, get all income from project
-        -- Get all income from all projects
+        
+        
         SELECT sum(salePrice) INTO totalProjectRevenue from bsa_revenue_item where (SELECT EXTRACT(YEAR FROM dateSold) FROM DUAL) = yearToCompute and ispending = 'N' and platformsoldon = 'Ebay';
-        -- Get income from single project
+        
         SELECT sum(salePrice) INTO projectRevenue from bsa_revenue_item where (SELECT EXTRACT(YEAR FROM dateSold) FROM DUAL) = yearToCompute and project_id = projectId and ispending = 'N' and platformsoldon = 'Ebay';
-        -- Get income from income table (outside base income)
+        
         SELECT ti.income INTO income from bsa_tax_income ti WHERE year = yearToCompute;
-        -- Get tax for base income (use isEstimated to determine if we should use estimate or actual income)
+        
         taxFromIncome := bsa_func_calculate_federal_income_tax_2021(income);
-        -- Get tax from income with revenue added on
+        
         taxFromIncomeWithRevenue := bsa_func_calculate_federal_income_tax_2021(income + totalProjectRevenue);
-        -- Divide tax by ratio of project income vs all projects
+        
         revenueRatio := projectRevenue / totalProjectRevenue;
         taxDueOnProject := taxDueOnProject + (revenueRatio * (taxFromIncomeWithRevenue - taxFromIncome));
-        -- Add to sum then iterate (return aggregate sum)
+        
         return taxDueOnProject;
     END;
 
-    -- Number of revenue items for 2022 must be greater than 0, otherwise the behavior of this function is undefined
+    
     function bsa_func_calculate_federal_tax_helper_2022 (projectId IN int)
     RETURN number
     AS
@@ -130,34 +130,34 @@ as
     BEGIN
         yearToCompute := '2022';
         taxDueOnProject := 0;
-        -- For each year, get all income from project
-        -- Get all income from all projects
+        
+        
         SELECT sum(salePrice) INTO totalProjectRevenue from bsa_revenue_item where (SELECT EXTRACT(YEAR FROM dateSold) FROM DUAL) = yearToCompute and ispending = 'N' and platformsoldon = 'Ebay';
-        -- Get income from single project
+        
         SELECT sum(salePrice) INTO projectRevenue from bsa_revenue_item where (SELECT EXTRACT(YEAR FROM dateSold) FROM DUAL) = yearToCompute and project_id = projectId and ispending = 'N' and platformsoldon = 'Ebay';
-        -- Get income from income table (outside base income)
+        
         SELECT ti.income INTO income from bsa_tax_income ti WHERE year = yearToCompute;
-        -- Get tax for base income 
+        
         taxFromIncome := bsa_func_calculate_federal_income_tax_2022(income);
-        -- Get tax from income with revenue added on
+        
         taxFromIncomeWithRevenue := bsa_func_calculate_federal_income_tax_2022(income + totalProjectRevenue);
-        -- Divide tax by ratio of project income vs all projects
+        
         revenueRatio := projectRevenue / totalProjectRevenue;
         taxDueOnProject := taxDueOnProject + (revenueRatio * (taxFromIncomeWithRevenue - taxFromIncome));
-        -- Add to sum then iterate (return aggregate sum)
+        
         return taxDueOnProject;
     END;
 
-    -- Each year will get a function based on the year's tax bracket
-    -- Note that income is expected to be "after any and all deductions"
-    -- Note: 2021 was filed as a single filer
+    
+    
+    
     function bsa_func_calculate_federal_income_tax_2021 (income IN number)
     RETURN number
     AS
         taxDue number(10, 2);
     BEGIN
         if income <= 0 THEN
-            -- If income is 0 (or negative, we return 0)
+            
             taxDue := 0;
         elsif income > 0 and income < 9950 THEN
             taxDue := .10 * income;
@@ -172,20 +172,20 @@ as
         elsif income > 209426 and income < 523600 THEN
             taxDue := ((income - 209426) * .35) + 47843;
         else
-            -- Anything over 523600
+            
             taxDue := ((income - 523600) * .37) + 157804.25;
         end if;
         return taxDue; 
     END;
 
-    -- Note: 2022 will be filed as married; jointly
+    
     function bsa_func_calculate_federal_income_tax_2022 (income IN number)
     RETURN number
     AS
         taxDue number(10, 2);
     BEGIN
         if income <= 0 THEN
-            -- If income is 0 (or negative, we return 0)
+            
             taxDue := 0;
         elsif income > 0 and income < 20550 THEN
             taxDue := .10 * income;
@@ -200,7 +200,7 @@ as
         elsif income > 431900 and income < 647851 THEN
             taxDue := ((income - 431900) * .35) + 98671;
         else
-            -- Anything over 523600
+            
             taxDue := ((income - 647851) * .37) + 174253.50;
         end if;
         return taxDue; 
